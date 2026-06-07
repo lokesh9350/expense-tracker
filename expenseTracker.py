ce771e74839745ba68f600be572b890c5203a095
lokesh9350 <lokeshsangdoya@gmail.com>
2026-06-07 09:52:40 +0530

    expenseTracker v03 - Added Search Expense Feature

--- expenseTracker.py
+++ expenseTracker.py
@@ -2,7 +2,8 @@ def menu():
    print("1.Add Expense")
    print("2.View Expenses")
    print("3.Total Expense")
-   print("4.Exit")
+   print("4.Search ")
+   print("5.Exit")
 
 
 def add_expense(iname,iprice):
@@ -11,6 +12,9 @@ def add_expense(iname,iprice):
     iname.append(einame)
     iprice.append(eiprice)
 def view_expense(iname,iprice):
+    if len(iname)<=0:
+        print("first add expense")
+        return
     for i in range(len(iname)):
         print("Item ",i+1,"Name :",iname[i])
         print("Item",i+1,"Price :",iprice[i])
@@ -19,7 +23,16 @@ def total_expense(iprice):
     for i in range(len(iprice)):
         tot=tot+iprice[i]
     
-    print("Total  :",sum)
+    print("Total  :",tot)
+
+def search_expense(iname,iprice):
+    sname=input("Enter Expense Name :")
+    for i in range(len(iname)):
+        if iname[i]==sname:
+            print("Item name :",sname)
+            print("Item Price",iprice[i])
+            return
+    print("Not Found")
 
 
 def main():
@@ -35,7 +48,7 @@ def main():
         elif ch==3:
             total_expense(iprice)
         elif ch==4:
-           search_expense()
+           search_expense(iname,iprice)
 
         elif ch==5:
             print("Thanks")
