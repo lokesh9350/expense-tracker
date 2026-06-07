0947719baf5004b5c5d5913a69ad12d8a35ed450
lokesh9350 <lokeshsangdoya@gmail.com>
2026-06-07 09:43:18 +0530

    Add total expense calculation to expense tracker

--- expenseTracker.py
+++ expenseTracker.py
@@ -1,7 +1,8 @@
 def menu():
    print("1.Add Expense")
    print("2.View Expenses")
-   print("3.Exit")
+   print("3.Total Expense")
+   print("4.Exit")
 
 
 def add_expense(iname,iprice):
@@ -13,6 +14,12 @@ def view_expense(iname,iprice):
     for i in range(len(iname)):
         print("Item ",i+1,"Name :",iname[i])
         print("Item",i+1,"Price :",iprice[i])
+def total_expense(iprice):
+    tot=0
+    for i in range(len(iprice)):
+        tot=tot+iprice[i]
+    
+    print("Total  :",sum)
 
 
 def main():
@@ -26,6 +33,11 @@ def main():
         elif ch==2:
             view_expense(iname,iprice)
         elif ch==3:
+            total_expense(iprice)
+        elif ch==4:
+           search_expense()
+
+        elif ch==5:
             print("Thanks")
             break
         else:
